4794fcdd0c42f01ed239ada7e9e004a212a991c3
derekbl17 <derekc1fill@gmail.com>
2025-02-17 12:56:00 +0200

    z

--- index.js
+++ index.js
@@ -1,22 +1,38 @@
 "use-strict"
 console.log("index.js initiated")
 
+const regLand=document.getElementById("regLand")
+const logLand=document.getElementById("logLand")
+
 const landingPage={
-    regHeadButton : ()=>{
-        console.log("Reg form selected")
+    formStatus : "login",
+    landButtons : ()=>{
+        if(self.formStatus==="login"){
+            console.log("Reg form selected")
+            document.getElementById("registration-form").style.display="block"
+            document.getElementById("login-form").style.display="none"
+            regLand.style.display="none"
+            logLand.style.display="block"
+            self.formStatus="register"
+        }else{
+            console.log("Login form selected")
+            document.getElementById("registration-form").style.display="none"
+            document.getElementById("login-form").style.display="block"
+            regLand.style.display="block"
+            logLand.style.display="none"
+            self.formStatus="login"
+        }
     },
-    logHeadButton : ()=>{
-        console.log("Login form selected")
-    }
 };
 
-document.getElementById("regLand").addEventListener("click",(e)=>{
+regLand.addEventListener("click",(e)=>{
     e.preventDefault()
-    landingPage.regHeadButton()
+    landingPage.landButtons()
 });
 
-document.getElementById("logLand").addEventListener("click",(e)=>{
+logLand.addEventListener("click",(e)=>{
     e.preventDefault()
-    landingPage.logHeadButton()
-})
-    
+    landingPage.landButtons()
+});
+
+landingPage.landButtons() // on page load activate form btn
