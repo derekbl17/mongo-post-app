2d9c15f2900b2e1ebc219dcc6c12b1d8d3f3c7b1
derekbl17 <derekc1fill@gmail.com>
2025-02-17 09:09:45 +0200

    za

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const app = express();
+app.use(express.json());
+require("dotenv").config();
+const connectingDB = require("./config/db.js");
+const { errorHandler } = require("./middleware/errorHandler.js");
+const cors = require("cors");
+connectingDB();
+app.use(cors());
+app.options("*", cors());
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/users", require("./routes/userRoutes.js"));
+app.use(errorHandler);
+app.use("/ads", require("./routes/adRoutes.js"));
+
+console.log("JWT: ", process.env.JWT_SALT)
+
+const PORT = process.env.PORT || 999;
+app.listen(PORT, () => console.log(`server is UP on port: ${PORT}`));
